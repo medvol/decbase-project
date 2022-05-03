cd7c83151d6756aa7655e7a6aa898ca04d40d3b9
medvol <95881208+medvol@users.noreply.github.com>
2022-05-03 18:38:46 +0000

    Deploying to gh-pages from @ medvol/decbase-project@af3840b0b09162446222249ce19735162de5cf92 🚀

--- src.01c2da74.js
+++ src.01c2da74.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"/home/runner/work/decbase-project/decbase-project/src/images/icon/ellipse-icon-new.svg":[["ellipse-icon-new.0198a39c.svg","FHHN"],"FHHN"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/decbase-project/src.93ce1d84.js.map
+//# sourceMappingURL=/decbase-project/src.01c2da74.js.map
